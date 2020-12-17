e3afac0b377321d6363428817c46024bd43274d6
IzzyCode98 <73586023+IzzyCode98@users.noreply.github.com>
2020-12-17 14:30:21 +0400

    Add files via upload

--- Conways_Life_Original_Rules.f
+++ Conways_Life_Original_Rules.f
@@ -11,31 +11,6 @@ array-address !
  1 - bmp-x-size @ * swap 1 - + array-address @ + c! ;
 
 
-{ --------------- words for counting live cells and exporting data--------------------}
-
-variable pop                                      { population variable }
-: clearpop 0 pop ! ;                              { Sets population variable to 0 }
-  clearpop
-
-: countpop array-size @ 0 do array-address @ i + c@ 1 = if 1 pop +! then loop ;
-
-variable pop-data-id                             { Create Variable to hold file id handle }
-
-: make-data-file                                  { Create a test file to read / write to  }
-  s" C:\Users\isabe\Downloads\Conways_Life_Helper_Code_Examples_V4\ConwaysLife\Diff_Array_Life\popul_data.dat" r/w create-file drop  { Create the file } 
-  pop-data-id !                                  { Store file handle for later use }
-;
-
-: close-data-file                                 { Close the file pointed to by the file  }
-  pop-data-id @                                  { handle.                                }
-  close-file drop
-; 
-
-: write-pop-data                                        { Write a series of integer numbers to a }               
-  pop @ (.) pop-data-id @ write-line drop                { looped structure.  File must be open   }                                                     { for R/W access first.                  }
-  ;
-
-
 { ------------ make pixel white for life (1) and black for dead (0) ------------ }
 
 : white-pixel ( addr n -- )
@@ -115,69 +90,6 @@ neighbour_array !
 endcase loop count_neighbour ;
 
 
-{ ------------ go-life displays the evolution of life from a given starting array, stops with key press ------------ }
-
-: go-life                           { Draw bmp to screen at variable pixel size       }
-  cr ." Starting stretch to window test " 
-  cr cr
-  New-bmp-Window-stretch              { Create new "stretch" window                     }
-  bmp-window-handle !                 { Store window handle                             }
-  begin                            { Begin update / display loop                     }
-  liveordead
-  bmp-address @ bmp-to-screen-stretch { Stretch .bmp to display window                  }
-  count_neighbour
-  next_gen
-  100 ms
-  key?                             { Break test loop on key press                    }
-  until                              { Delay for viewing ease, reduce for higher speed }
-  cr ." Ending stretch to window test " 
-  cr cr
-  ;
-
-
-{ ------------ go-data displays the evolution of life from a given starting array and exports population data, stops after 1000 generations ------------ }
-
-variable gen_counter
-
-: go-data                           { Draw bmp to screen at variable pixel size       }
-  0 gen_counter !
-  make-data-file
-  cr ." Starting stretch to window test " 
-  cr cr
-  New-bmp-Window-stretch              { Create new "stretch" window                     }
-  bmp-window-handle !                 { Store window handle                             }
-  begin                            { Begin update / display loop                     }
-  clearpop countpop                { sets population variable to zero and re-counts }
-  write-pop-data
-  liveordead
-  bmp-address @ bmp-to-screen-stretch { Stretch .bmp to display window                  }
-  count_neighbour
-  next_gen
-  1 gen_counter +!
-  gen_counter @ 1000 =                             { Break test loop after 1000 generations}
-  until                             
-  cr ." Ending stretch to window test " 
-  cr cr
-  close-data-file ;
-
-: go-data-only                           
-  0 gen_counter !
-  make-data-file
-  cr ." Starting data collection " 
-  cr cr
-  begin                            { Begin update / display loop                     }
-  clearpop countpop                { sets population variable to zero and re-counts }
-  write-pop-data
-  liveordead
-  count_neighbour
-  next_gen
-  1 gen_counter +!
-  gen_counter @ 1000 =                             { Break test loop after 1000 generations}
-  until                             
-  cr ." Ending data collection " 
-  cr cr
-  close-data-file ;
-
 { ------------ useful words for troubleshooting and seeing data ------------ }
 
 : reset_neighbour neighbour_array @ array-size @ erase 0 neibor_var ! ;  
@@ -188,36 +100,3 @@ variable gen_counter
 
 : clearstack depth 0 > if begin drop depth 0 = until then ;
 
-{ ------------ do all data in 1 ------------ }
-
-variable life%
-
-: random-life-%
-  array-size @ 0 do
-  100 RND
-  life% @ < if 1 
-  else 0 then
-  array-address @ i + c! loop ;
-
-
-: go-data-all
-  make-data-file
-  cr ." Starting data collection " 
-  cr cr
-  101 0 do i life% !
-  reset_array
-  random-life-%                           
-  0 gen_counter !  
-  begin                            { Begin update / display loop                     }
-  clearpop countpop                { sets population variable to zero and re-counts }
-  write-pop-data
-  liveordead
-  count_neighbour
-  next_gen
-  1 gen_counter +!
-  gen_counter @ 1000 =                             { Break test loop after 1000 generations}
-  until  
-  loop                           
-  cr ." Ending data collection " 
-  cr cr
-  close-data-file ;
---
+++ Export_and_Display.f
@@ -0,0 +1,125 @@
+{ --------------- words for counting live cells and exporting data--------------------}
+
+variable pop                                      { population variable }
+: clearpop 0 pop ! ;                              { Sets population variable to 0 }
+  clearpop
+
+: countpop array-size @ 0 do array-address @ i + c@ 1 = if 1 pop +! then loop ;
+
+variable pop-data-id                             { Create Variable to hold file id handle }
+
+: make-data-file                                  { Create a test file to read / write to  }
+  s" C:\Users\isabe\Downloads\Conways_Life_Helper_Code_Examples_V4\ConwaysLife\Diff_Array_Life\popul_data.dat" r/w create-file drop  { Create the file } 
+  pop-data-id !                                  { Store file handle for later use }
+;
+
+: close-data-file                                 { Close the file pointed to by the file  }
+  pop-data-id @                                  { handle.                                }
+  close-file drop
+; 
+
+: write-pop-data                                        { Write a series of integer numbers to a }               
+  pop @ (.) pop-data-id @ write-line drop                { looped structure.  File must be open   }                                                     { for R/W access first.                  }
+  ;
+
+
+{ ------------ go-life displays the evolution of life from a given starting array, stops with key press ------------ }
+
+: go-life                           { Draw bmp to screen at variable pixel size       }
+  cr ." Starting stretch to window test " 
+  cr cr
+  New-bmp-Window-stretch              { Create new "stretch" window                     }
+  bmp-window-handle !                 { Store window handle                             }
+  begin                            { Begin update / display loop                     }
+  liveordead
+  bmp-address @ bmp-to-screen-stretch { Stretch .bmp to display window                  }
+  count_neighbour
+  next_gen
+  100 ms			{ Delay for viewing ease, reduce for higher speed }
+  key?                             { Break test loop on key press                    }
+  until                              
+  cr ." Ending stretch to window test " 
+  cr cr
+  ;
+
+
+{ ------------ go-data displays the evolution of life from a given starting array and exports population data, stops after 1000 generations ------------ }
+
+variable gen_counter
+
+: go-data                           { Draw bmp to screen at variable pixel size       }
+  0 gen_counter !
+  make-data-file
+  cr ." Starting stretch to window test " 
+  cr cr
+  New-bmp-Window-stretch              { Create new "stretch" window                     }
+  bmp-window-handle !                 { Store window handle                             }
+  begin                            { Begin update / display loop                     }
+  clearpop countpop                { sets population variable to zero and re-counts }
+  write-pop-data
+  liveordead
+  bmp-address @ bmp-to-screen-stretch { Stretch .bmp to display window                  }
+  count_neighbour
+  next_gen
+  1 gen_counter +!
+  gen_counter @ 1000 =                             { Break test loop after 1000 generations}
+  until                             
+  cr ." Ending stretch to window test " 
+  cr cr
+  close-data-file ;
+
+
+{ ------------ go-data-only tracks the evolution of life from a given starting array and exports population data, stops after 1000 generations, does not open display ------------ }
+
+: go-data-only                           
+  0 gen_counter !
+  make-data-file
+  cr ." Starting data collection " 
+  cr cr
+  begin                            { Begin update / display loop                     }
+  clearpop countpop                { sets population variable to zero and re-counts }
+  write-pop-data
+  liveordead
+  count_neighbour
+  next_gen
+  1 gen_counter +!
+  gen_counter @ 1000 =                             { Break test loop after 1000 generations}
+  until                             
+  cr ." Ending data collection " 
+  cr cr
+  close-data-file ;
+
+
+{ ------------ go-data-all tracks the evolution of life and exports population data, stops after 1000 generations and repeats for every integer % of a starting population, does not open display ------------ }
+
+variable life%
+
+: random-life-%
+  array-size @ 0 do
+  100 RND
+  life% @ < if 1 
+  else 0 then
+  array-address @ i + c! loop ;
+
+
+: go-data-all
+  make-data-file
+  cr ." Starting data collection " 
+  cr cr
+  101 0 do i life% !
+  reset_array
+  random-life-%                           
+  0 gen_counter !  
+  begin                            { Begin update / display loop                     }
+  clearpop countpop                { sets population variable to zero and re-counts }
+  write-pop-data
+  liveordead
+  count_neighbour
+  next_gen
+  1 gen_counter +!
+  gen_counter @ 1000 =                             { Break test loop after 1000 generations}
+  until  
+  loop                           
+  cr ." Ending data collection " 
+  cr cr
+  close-data-file ;
